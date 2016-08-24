fb4eb381119bab34229b28dd0652770c6abc5c0b
Siva Krishnan <sivakrishnan@live.com>
2016-08-24 15:02:41 +0530

    join fix

--- src/actions/Registration/index.js
+++ src/actions/Registration/index.js
@@ -271,7 +271,7 @@ function postActionConstruct(json, isIndividual) {
       }
 
       dispatch(LoginActions.loginUser(username, password));
-      dispatch(LoginActions.submitLogin(getState().orgs.addOrg, !isIndividual));          
+      dispatch(LoginActions.submitLogin(getState().orgs.addOrg));          
 
       if (typeof(Storage) !== "undefined") {
         localStorage.setItem("user_channel", payload.channel);
