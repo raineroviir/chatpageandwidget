ff71f2e51ab0b0c0290b6950d3b09acd2a64535f
Rainer <roviir@gmail.com>
2016-09-28 20:43:45 -0700

    widget moved

---
+++ src/widget.js
@@ -0,0 +1,18 @@
+import 'babel-polyfill'
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import configureStore from './store/configureStore'
+import App from './Containers/App'
+
+const preloadedState = window.__PRELOADED_STATE__
+const store = configureStore(preloadedState)
+const rootElement = document.getElementById('chat-center-widget')
+const channel_url = rootElement.getAttribute('chat-channel-url')
+
+render(
+  <Provider store={store}>
+    <App channel_url={channel_url} />
+  </Provider>,
+  rootElement
+)
