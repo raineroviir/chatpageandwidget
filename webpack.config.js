7c24e26c9338f07867ce0f51dcd7cf45ea6134f9
Rainer <roviir@gmail.com>
2016-10-10 22:45:24 -0700

    fix es2015 presets

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
         loader: 'babel',
         exclude: /node_modules/,
         query: {
-          presets: [ 'react-hmre' ]
+          presets: [ 'react-hmre', "react", "es2015" , "stage-0" ]
         }
       },
       {
