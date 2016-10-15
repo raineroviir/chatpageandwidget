36015eff3650ea55f378d57de82f4383df50e24e
Rainer <roviir@gmail.com>
2016-10-14 18:28:07 -0700

    fix capture details page

--- src/Containers/ChatWidget/Header.js
+++ src/Containers/ChatWidget/Header.js
@@ -70,32 +70,36 @@ class Header extends Component {
     const teamChannelUrl = widget.initialConfig.channelUrl ||  "seaShells.com"
     const welcomeMessage = widget.initialConfig.content ? widget.initialConfig.content.welcomeMessage : "Hi there, thanks for checking out Chat Center, if you have any questions we will be happy to help, just let us know"
     const teamName = widget.initialConfig.content ? widget.initialConfig.content.teamName : ""
+    const currentUserEmail = guest.guest ? guest.data.email : user.data.email
+    const currentUserName = guest.guest ? guest.data.first_name : user.data.first_name
     const enterInformationForNotificationsModal =
-      <div className="your-details-entry-form">
+      (<div className="your-details-entry-form">
         <div style={{padding: "10px", display: "flex", "flexDirection": "column"}}>
         <div onClick={this.enterEmailForNotificationsToggle} style={{cursor: "pointer", alignSelf: 'flex-end', width: "48px", height: "48px", backgroundImage: `url(${closeIcon})`}}></div>
-        <div className="your-details">
+        <div className="your-details" style={{padding: "10 0 10 0"}}>
           Your Details
         </div>
-          <form onSubmit={this.handleUserUpdate} style={{padding: "0 40 0 0"}}>
-            <div style={{padding: "0 40 0 10", letterSpacing: "0.1px"}}>
+          <form onSubmit={this.handleUserUpdate} style={{fontSize: "15px"}}>
+            <div style={{padding: "10px", letterSpacing: "0.1px"}}>
               Name or nickname
             </div>
-            <input ref="nameCapture" style={{padding: "0 0 0 10", opacity: "0.6", letterSpacing: "0.1px", width: "100%", border: "none", height: "30px", borderBottom: "2px solid"}} placeholder="Enter your name"/>
-            <div>
-            <div style={{padding: "0 40 0 10", letterSpacing: "0.1px"}}>
+            <div style={{padding: "10px"}}>
+              <input className="capture-details-input" ref="nameCapture" style={{fontSize: "15px", letterSpacing: "0.1px", width: "100%", border: "none", height: "30px", borderBottom: "2px solid"}} placeholder={currentUserName ? currentUserName : "Enter your name"}/>
+            </div>
+            <div style={{padding: "10px", letterSpacing: "0.1px"}}>
               Email for notifications
             </div>
-            <input ref="emailCapture" style={{padding: "0 0 0 10", opacity: "0.6", letterSpacing: "0.1px", width: "100%", border: "none", borderBottom: "2px solid", height: "30px"}}  placeholder="Enter your email"/>
+            <div style={{padding: "10px"}}>
+              <input className="capture-details-input" ref="emailCapture" style={{fontSize: "15px", letterSpacing: "0.1px", width: "100%", border: "none", borderBottom: "2px solid", height: "30px"}}  placeholder={currentUserEmail ? currentUserEmail : "Enter your email"}/>
             </div>
-            <div style={{padding: "10px 0 0 0"}}>
-            <button type="submit" style={{padding: "0 10 0 0", backgroundColor: widget.initialConfig.keyColor, height: "48px", borderRadius: "5px", color: "#FFFFFF", display: "flex", justifyContent: "center", width: "200px"}}>
-              <div style={{alignSelf: "center"}}>Save Changes</div>
-            </button>
+            <div style={{padding: "20px 0 0 0"}}>
+              <button type="submit" style={{ backgroundColor: widget.initialConfig.keyColor, height: "48px", borderRadius: "5px", color: "#FFFFFF", display: "flex", justifyContent: "center", width: "100%", borderStyle: "none"}}>
+                <div style={{alignSelf: "center", fontSize: "15px"}}>Save Changes</div>
+              </button>
             </div>
           </form>
         </div>
-      </div>
+      </div>)
     const menu = (
         <div style={{color: widget.initialConfig.keyColor}} className="menu">
           <div className="menu-popup-triangle"><GoTriangleUp /></div>
