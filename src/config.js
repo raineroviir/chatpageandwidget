53077279834074b20ead97139d063349228a2840
Kiran <kirankumar@bpatech.com>
2016-11-07 02:20:00 +0530

    kiran test commit for new branch

--- src/config.js
+++ src/config.js
@@ -9,6 +9,7 @@ window.config = {
 var Config = {
     authBase: 'http://ec2-54-169-64-117.ap-southeast-1.compute.amazonaws.com:2222/oauth/token',
     widget: "http://ec2-54-169-64-117.ap-southeast-1.compute.amazonaws.com:3333/v1",
+    chatpage: "http://ec2-54-169-64-117.ap-southeast-1.compute.amazonaws.com:3333/v1",
     api: 'http://ec2-54-169-64-117.ap-southeast-1.compute.amazonaws.com:1111/v1',
 };
 export default Config; 
